697116041845c968ec6bc4246775d66e476fb2ef
Sergey <atljhfirj2015@gmail.com>
2024-11-27 23:07:04 +0300

    step

--- localstorage-manager/src/Popup.tsx
+++ localstorage-manager/src/Popup.tsx
@@ -8,16 +8,25 @@ import "./styles/popup.css";
 
 const Popup: React.FC = () => {
 	const { items, addItem, updateItem, deleteItem } = useLocalStorage();
-	const [isAddFormVisible, setAddFormVisible] = useState(false);
+	// const [isAddFormVisible, setAddFormVisible] = useState(false);
 
 	return (
 		<div className="popup-container">
-			<h3>LocalStorage Manager</h3>
-			<LocalStorageList items={items} onDelete={deleteItem} />
-			<ToggleAddFormButton isVisible={isAddFormVisible} onClick={() => setAddFormVisible((prev) => !prev)} />
-			{isAddFormVisible && <LocalStorageForm onAdd={addItem} />}
-			<LocalStorageEditor items={items} onUpdate={updateItem} />
+			<div className="viewer-container">
+				<LocalStorageList items={items} onDelete={deleteItem} />
+			</div>
+			<div className="controls-container">
+				<LocalStorageForm onAdd={addItem} />
+				<LocalStorageEditor items={items} onUpdate={updateItem} />
+			</div>
 		</div>
+		//
+		// <div className="popup-container">
+		// 	<LocalStorageList items={items} onDelete={deleteItem} />
+		// 	<ToggleAddFormButton isVisible={isAddFormVisible} onClick={() => setAddFormVisible((prev) => !prev)} />
+		// 	{isAddFormVisible && <LocalStorageForm onAdd={addItem} />}
+		// 	<LocalStorageEditor items={items} onUpdate={updateItem} />
+		// </div>
 	);
 };
 
